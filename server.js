507843dd522b1748b2a1834efd04c2c167c39a82
23019609 <23019609@myrp.edu.sg>
2026-01-10 18:02:58 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+// include the required packages
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+// database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+}
+
+// intialize Express app
+const app = express();
+// helps app to read JSON
+app.use(express.json());
+
+// start the server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+// Example Route: Get all movies
+app.get('/allmovies', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.movies');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for allmovies'});
+    }
+});
+
+// Example Route: Create a new movie
+app.post('/addmovie', async(req, res) => {
+    const {movie_name, movie_year, movie_pic} = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO movies (movie_name, movie_year, movie_pic) VALUES (?, ?, ?)', [movie_name, movie_year, movie_pic]);
+        res.status(201).json({message: 'Card ' + movie_name + ' added successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not add movie ' + movie_name});
+    }
+})
